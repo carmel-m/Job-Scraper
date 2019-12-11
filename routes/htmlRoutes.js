5d2fc2b29d0f0e31d950f0f89e6e3fe66e463f77
Carmel Meyer <cemeyer@elkera.com>
2019-12-11 14:21:05 -0500

    Initial file setup

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+// check cats app
+
+// append articles in handlebars doc
+// jquery for onClicks but don't use jquery to loop over and append
+
+// 2x front end routes
+
+// need a "/"" route and "/saved"
+
+
+// get scrape button working early
